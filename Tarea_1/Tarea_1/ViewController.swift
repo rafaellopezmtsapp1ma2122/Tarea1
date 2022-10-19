13a2378c7dc312eaee371180ce259408671d5e9e
Rafa <rafael_lopez_m-tsapp1ma2122@cev.com>
2022-10-19 11:51:40 +0200

    Creación del proyecto

---
+++ Tarea_1/Tarea_1/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Tarea_1
+//
+//  Created by Apps2M on 19/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        print("hola")
+    }
+
+   
+
+}
+
